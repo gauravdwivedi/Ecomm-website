ecf44e3ad24a55f9569ef445bfaa0d0441ce06f9
Gaurav Dwivedi <gaurav@vividwebtechnologies.com>
2022-06-08 02:34:12 +0530

    Detail Page UI Fixes

--- src/app/components/Mobile/Detail.jsx
+++ src/app/components/Mobile/Detail.jsx
@@ -114,6 +114,9 @@ const Detail = React.memo(function Detail(props) {
 
 	return (
 		<div id="main">
+			<div className='top-shadow'>
+
+			</div>
 			<div className="detail-header">
 				<Link to="/">
 					<img src="/images/back.svg" className="img-fluid" alt="" />
@@ -131,59 +134,79 @@ const Detail = React.memo(function Detail(props) {
 				<VedioPlayer url={props.detail?.videos[0]?.url} />
 				: ""}
 
-			<div className="video-content d-flex flex-column w-100 mw-100 overflow-hidden" style={{ padding: '20px' }}>
-				<div className='d-flex  justify-content-between mw-90'>
-					<div className="caption" >
-						<div className="contents d-flex justify-content-between" >
-							<div style={{ maxWidth: '70%' }}>
-								<h2>{props.detail.title}</h2>
-								<div className="price">
-									{(props.detail.attributes && props.detail?.attributes.length > 0) ?
-										<h4>{'$' + props.detail.attributes[0].discounted_price} <del>{'$' + props.detail.attributes[0].price}</del></h4>
-										: ""}
-								</div>
-								<ul className="ratings">
-									{new Array(5).fill("", 0, 5).map((p, i) => (i < props.detail.rating) ?
-										<li key={i}><i className="iconly-Star icbo"></i></li> :
-										<li key={i}><i className="iconly-Star icbo empty"></i></li>)}
-								</ul>
-								<p>{props.detail.description}</p>
+			{/* <div className="video-content d-flex flex-column w-100 mw-100 overflow-hidden" style={{ padding: '20px' }}> */}
+			<div className="video-content" >
+				{/* <div className='d-flex  justify-content-between mw-90'> */}
+				<div className="caption" >
+					<div className='contnt'>
+						<div className='contents' >
+							<h2>{props.detail.title}</h2>
+							<div className="price">
+								{(props.detail.attributes && props.detail?.attributes.length > 0) ?
+									<h4>{'$' + props.detail.attributes[0].price + '.00'} <del>{'$' + props.detail.attributes[0].discounted_price + '.00'}</del></h4>
+									: ""}
+							</div>
+							<ul className="ratings">
+								{new Array(5).fill("", 0, 5).map((p, i) => (i < props.detail.rating) ?
+									<li key={i}><i className="iconly-Star icbo" ></i></li> :
+									<li key={i}><i className="iconly-Star icbo empty"></i></li>)}
+							</ul>
+							<p>{props.detail.description}</p>
 
-								<div className="detail-gallary" style={{ display: 'inline-block', maxWidth: '60%', overflow: 'auto' }}>
-									<ul className='detail__gallery-li d-flex overflow-scroll'>
-										{props.detail.images && images.map((item, i) => <li key={i}><img src={config.IMG_END_POINT + item} alt="" className='detail__gallary-img' /></li>)}
-									</ul>
-								</div>
-								<div className="hrs-btn">
-									<span className='hrs-text'><img src="/images/hr-icon.svg" alt="" /><h6>Delivery : With in 3 Hrs</h6></span>
-								</div>
+							<div className="detail-gallary">
+								<ul className='detail__gallary'>
+									{props.detail.images && images.map((item, i) => <li key={i}><img src={config.IMG_END_POINT + item} alt="" className='detail__gallary-img' /></li>)}
+								</ul>
+							</div>
+							<div className="hrs-btn">
+								<span className='hrs-text'><img src="/images/hr-icon.svg" alt="" /><h6>Delivery : With in 3 Hrs</h6></span>
 							</div>
 						</div>
 					</div>
-					<div className="contents right-sec " >
-						<ul className='d-flex flex-column justify-content-around flex-fill h-100 '>
-							<li>
-								{fav ? <img src="/images/detail-video/icon/notify-red.svg" alt="" onClick={handleFavClick} /> : <img src="/images/detail-video/icon/notify.svg" alt="" onClick={handleFavClick} />}
-							</li>
-							<li className='liked'>
-								{like ? <>
-									<img src="/images/detail-video/icon/unlike.svg" alt="" onClick={handleLikeClick} />
-									<span style={{ color: 'white', fontSize: '16px', alignItems: 'center', margin: 'auto' }}>{noOfLikes}</span>
-								</> :
-									<>
-										<img src="/images/detail-video/icon/like.svg" alt="" onClick={handleLikeClick} />
-										<span style={{ color: 'white', fontSize: '16px', margin: 'auto' }}>{noOfLikes}</span>
-									</>}
-							</li>
-							<li><img src="/images/detail-video/icon/message.svg" alt="" /></li>
-							<li><img src="/images/detail-video/icon/share.svg" alt="" /></li>
-						</ul>
+					<div className="" >
+						<div className='right-sec'>
+							<ul>
+								<li>
+									{fav ? <img src="/images/detail-video/icon/notify-red.svg" alt="" onClick={handleFavClick} /> : <img src="/images/detail-video/icon/notify.svg" alt="" onClick={handleFavClick} />}
+								</li>
+								<li className='liked'>
+									{like ? <>
+										<img src="/images/detail-video/icon/unlike.svg" alt="" onClick={handleLikeClick} />
+										<span className='like-item-1'>{noOfLikes}</span>
+									</> :
+										<>
+											<img src="/images/detail-video/icon/like.svg" alt="" onClick={handleLikeClick} />
+											<span className='like-item'>{noOfLikes}</span>
+										</>}
+								</li>
+								<li><img src="/images/detail-video/icon/message.svg" alt="" /></li>
+								<li><img src="/images/detail-video/icon/share.svg" alt="" /></li>
+							</ul>
+						</div>
 					</div>
-				</div >
+				</div>
+
 
 				<Toaster />
+				<div className="detail-add">{context.isAuthenticated ? <>{inCart == true ?
+					<div>
+						<Link to={{ pathname: "/cart", state: { fromProductPage: true } }} className="btn btn-solid flex-fill" >Go to cart</Link>
+					</div>
+					: <>{
+						modalShow ? <> <h4 className='text-white'>Select Size</h4>
+							<div>
+							</div> <button onClick={handleInsertInCart} className="btn btn-solid flex-fill" tabIndex="0">Add To Cart</button></>
+							: <button onClick={handleAddCart} className="btn btn-solid flex-fill" tabIndex="0">Add To Cart</button>
+					}
+					</>}</> : <button onClick={handleAddCart} className="btn btn-solid flex-fill" tabIndex="0">Login</button>}
+				</div>
+				<div className='bottom-shadow'>
+				</div>
+				{/* </div > */}
 
-				<div className="detail-add flex-fill" style={{ borderRadius: '20px' }}>{context.isAuthenticated ? <>{inCart == true ?
+
+
+				{/* <div className="detail-add flex-fill" style={{ borderRadius: '20px' }}>{context.isAuthenticated ? <>{inCart == true ?
 					<div>
 
 						<Link to={{ pathname: "/cart", state: { fromProductPage: true } }} className="btn btn-solid flex-fill" >Go to cart</Link>
@@ -195,11 +218,10 @@ const Detail = React.memo(function Detail(props) {
 							: <button onClick={handleAddCart} className="btn btn-solid flex-fill" tabIndex="0">Add To Cart</button>
 					}
 					</>}</> : <button onClick={handleAddCart} className="btn btn-solid flex-fill" tabIndex="0">Login</button>}
-				</div>
-
+				</div> */}
 			</div >
-		</div >
 
+		</div >
 	)
 })
 
@@ -272,4 +294,5 @@ const VedioPlayer = ({ url }) => {
 // 		</div>
 // 	)
 // }
+
 export default Detail
