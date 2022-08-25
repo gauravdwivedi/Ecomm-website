09341db78f3300e93d15b0d91f52891313494770
Gaurav Dwivedi <gaurav@vividwebtechnologies.com>
2022-08-25 19:04:08 +0530

    Order Pagination

--- src/app/components/Mobile/Order.jsx
+++ src/app/components/Mobile/Order.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
+import { useEffect } from 'react';
 import OrderItem from './OrderItem'
+import Pagination from './Pagination'
+// import data from './mock-data.json'
 
+
+let PageSize = 5;
 function Order(props) {
 
-    // console.log('ORDERS', props?.order?.getOrderList.result)
+    console.log('ORDERS', props?.order?.getOrderList?.result)
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [isDataLoaded, setIsDataLoaded] = useState(false);
+    const [len, setLen] = useState(0);
+    const [data, setData] = useState(null);
+    // let currentTableData;
+    // const [currentTableData, setCurrentTableData] = useState(null);
+
+    // useEffect(() => {
+    //     if (props?.order?.getOrderList?.result) {
+    //         const firstPageIndex = (currentPage - 1) * PageSize;
+    //         const lastPageIndex = firstPageIndex + PageSize;
+
+    //         setCurrentTableData(props.order.getOrderList.result.slice(firstPageIndex, lastPageIndex))
+
+    //     }
+    // }, [currentPage])
+
+    useEffect(() => {
+        if (props?.order?.getOrderList?.result) {
+            setData(props?.order?.getOrderList?.result);
+            setIsDataLoaded(true);
+        }
+    })
+
+
+    const currentTableData = () => {
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+
+        const res = data.slice(firstPageIndex, lastPageIndex);
+        console.log(res, 'RESPONSE')
+        return res
+    }
+
+
+
+
+    // const currentTableData = useMemo(() => {
+    //     const firstPageIndex = (currentPage - 1) * PageSize;
+    //     const lastPageIndex = firstPageIndex + PageSize;
+
+    //     return props?.order?.getOrderList.result ? props?.order?.getOrderList.result.slice(firstPageIndex, lastPageIndex) : undefined;
+    // }, [currentPage]);
+
     return (
         <div id="main">
             <header>
@@ -19,11 +69,22 @@ function Order(props) {
 
             <section className="order-section pt-4 px-15">
 
-                {props?.order?.getOrderList.result ? props?.order?.getOrderList.result.map((item) => (
+                {/* {props?.order?.getOrderList.result ? props?.order?.getOrderList.result.map((item) => (
                     < OrderItem key={item.id} item={item} />
+                )) : <h3>Oops!!!...Nothing here!!</h3>} */}
+
+                {isDataLoaded ? currentTableData().map(item => (
+                    <OrderItem key={item.id} item={item} />
                 )) : <h3>Oops!!!...Nothing here!!</h3>}
 
             </section>
+            <Pagination
+                className="pagination-bar"
+                currentPage={currentPage}
+                totalCount={data && data.length}
+                pageSize={PageSize}
+                onPageChange={page => setCurrentPage(page)}
+            />
         </div>
     )
 }
